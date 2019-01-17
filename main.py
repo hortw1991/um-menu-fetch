6ebc08a925ee472e79484722677033f848ddd5e8
Thomas Horn <hortw1991@gmail.com>
2019-01-17 15:57:06 -0600

    added hours and command line args

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+"""
+Fetches a simple text menu for the day.  Currently only supports today's menu.
+
+Cmd Line Arguments:
+	None (or invalid): returns lunch if time < 4:30pm otherwise dinner
+	Hours: returns a list of all hours
+	Lunch: returns lunch menu
+	Dinner: returns dinner menu
+	Breakfast: returns breakfast menu
+"""
+
 import requests
 import time
+import sys
 from bs4 import BeautifulSoup
 
 
-def format_url():
-	""" 
+def format_url(flag=None):
+	"""
 	Returns a formatted URL for lunch or dinner with current date.
 	"""
 
@@ -26,16 +38,23 @@ def format_url():
 		month = month.strip('0')
 	if day.startswith('0'):
 		day = day.strip('0')
-	
+
 	# Set lunch or dinner flag
 	cur_time = int(time.strftime('%H'))
 
-	if cur_time < 1630:
+	# Check for lunch override
+	if flag == 'lunch':
+		meal = lunch
+	elif flag == 'dinner':
+		meal = dinner
+	elif flag == 'breakfast':  # hidden option
+		meal = '1681'
+	elif cur_time < 1630:
 		meal = lunch
 	else:
 		meal = dinner
-	
-	return url.format(meal, month, day, year)	
+
+	return url.format(meal, month, day, year)
 
 
 def get_request(url):
@@ -55,20 +74,46 @@ def parse_contents(content):
 	return [item.text for item in c]
 
 
+def print_menu(food):
+	""" Prints general and daily info. """
+	for item in food: print(item)
 
 
+def display_hours():
+	""" Displays information about location and hours of all fooderies. """
+	with open('tiger_den_hours.txt', 'r') as hours:
+		for line in hours: print(line.strip())
 
 def main():
-	url = format_url()
+	"""
+	Checks for any optional arg and displays that requested info
+	"""
+	flag = ''
+
+	try:
+		if sys.argv[1].lower() == 'hours':
+			display_hours()
+			quit()
+		elif sys.argv[1].lower() == 'lunch':
+			flag = 'lunch'
+		elif sys.argv[1].lower() == 'dinner':
+			flag = 'dinner'
+		elif sys.argv[1].lower() == 'breakfast':
+			flag = 'breakfast'
+		
+	except IndexError:
+		flag = None
+
+	url = format_url(flag)
 	r = get_request(url)
 	if not r:
 		raise ConnectionError
-	
-	food = parse_contents(r.content)
 
-	for item in food: print(item)
+	print_menu(parse_contents(r.content))
+
 
 
 
 if __name__ == "__main__":
 	main()
+
